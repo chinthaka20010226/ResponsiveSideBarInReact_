2aba716f94a32ce685bfcaa2e02435dde0ac6d7a
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-24 02:20:33 +0530

    Router -> Routes -> Route.... Components...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './Components/Sidebar';
 // import logo from './logo.svg';
 // import './App.css';
+import Dashboard from './Pages/Dashboard';
+import About from './Pages/About';
+import Comment from './Pages/Comment';
+import Analytics from './Pages/Analytics';
+import Product from './Pages/Product';
+import ProductList from './Pages/ProductList';
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </Router>
   );
 }
 
