9de7a9f871a5c8a8ace51d1a34b67a5774f674c7
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-24 01:13:32 +0530

    Now, start the Route...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
+import Sidebar from './Components/Sidebar';
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Sidebar />
     </div>
   );
 }
---
+++ src/Pages/ProductList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductList(){
+    return(
+        <div>
+            <h1>ProductList page</h1>
+        </div>
+    )
+}
+
+export default ProductList;
