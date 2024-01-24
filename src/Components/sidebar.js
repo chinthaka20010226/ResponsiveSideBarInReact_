d4e3df9457211540281e9a9eed4032d11bb4af64
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-24 17:33:09 +0530

    create object Array, ???

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { FaTh, FaBars } from 'react-icons/fa';
 
 function Siedebar(){
+    const menuItems = [
+        {
+            path:"/",
+            name:"Dashboard",
+            icon:<FaTh />
+        },
+        {
+            path:"/about",
+            name:"Dashboard",
+            icon:<FaTh />
+        },
+        {
+            path:"/analytics",
+            name:"Analytics",
+            icon:<FaTh />
+        },
+        {
+            path:"/comment",
+            name:"Comment",
+            icon:<FaTh />
+        },
+        {
+            path:"/product",
+            name:"Product",
+            icon:<FaTh />
+        },
+        {
+            path:"/productList",
+            name:"ProductList",
+            icon:<FaTh />
+        }
+    ]
+
     return(
-        <div>
-            <h1>this is Sidebar</h1>
-        </div>
+        <div></div>
     )
 }
 
