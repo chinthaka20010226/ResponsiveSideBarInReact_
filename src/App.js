e608cd750f91022a1b78e89cf1e1c9fafd68004c
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-24 18:44:36 +0530

    start the style...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import Sidebar from './Components/Sidebar';
 import Dashboard from './Pages/Dashboard';
 import About from './Pages/About';
