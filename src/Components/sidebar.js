281d615f55ca3a460c8b6fafd2075c0e8cde8a17
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-24 22:59:53 +0530

    completed responsive sidebar...

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,43 +1,60 @@
-import React from 'react';
-import { FaTh, FaBars } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { 
+    FaTh, 
+    FaBars, 
+    FaUserAlt, 
+    FaRegChartBar, 
+    FaCommentAlt, 
+    FaShoppingBag, 
+    FaThList 
+} from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 
 const Siedebar = ({children}) => {
+    const[isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     const menuItems = [
         {
-            path:"/about",
+            path:"/dashboard",
             name:"Dashboard",
             icon:<FaTh />
         },
         {
+            path:"/about",
+            name:"About",
+            icon:<FaUserAlt />
+        },
+        {
             path:"/analytics",
             name:"Analytics",
-            icon:<FaTh />
+            icon:<FaRegChartBar />
         },
         {
             path:"/comment",
             name:"Comment",
-            icon:<FaTh />
+            icon:<FaCommentAlt />
         },
         {
             path:"/product",
             name:"Product",
-            icon:<FaTh />
+            icon:<FaShoppingBag />
         },
         {
             path:"/productList",
             name:"ProductList",
-            icon:<FaTh />
+            icon:<FaThList />
         }
     ]
 
     return(
         <div className='container'>
-            <div className='sidebar'>
+            <div style={{width: isOpen ? "250px" : "50px"}} className='sidebar'>
                 <div className='top-section'>
-                    <h1 className='logo'>Logo</h1>
-                    <div className='bar'>
-                        <FaBars />
+                    <h1 style={{display: isOpen ? "block" : "none"}} className='logo'>Logo</h1>
+                    <div style={{marginLeft: isOpen ? "30px" : "0px", paddingLeft: isOpen ? "40px" : "0px"}} className='bars'>
+                        <FaBars onClick={toggle} />
                     </div>
                 </div>
                 <div className='body-section'>
@@ -45,7 +62,7 @@ const Siedebar = ({children}) => {
                         menuItems.map((item, index) => (
                             <NavLink to={item.path} key={index} className="link" activeclassName="active">
                                 <div className='icon'>{item.icon}</div>
-                                <div className='link-text'>{item.name}</div>
+                                <div style={{display: isOpen ? "block" : "none"}} className='link-text'>{item.name}</div>
                             </NavLink>
                         ))
                     }
